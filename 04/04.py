c77680766a32bd8639d7b2a8caae76cf7e9bea2f
zephyruslove <zyzengstorm@gmail.com>
2015-10-04 16:37:10 +0800

    python challenges

---
+++ 04/04.py
@@ -0,0 +1,22 @@
+#!/bin/bash
+#coding=utf-8
+import re,urllib,sys
+
+#每次中断请修改nothing，然后keep going~
+nothing="12345"
+base_url='http://www.pythonchallenge.com/pc/def/linkedlist.php?nothing='
+url=base_url+nothing
+#print html
+count=1
+while nothing:
+    try:
+        html=urllib.urlopen(url).read()
+        patt=re.findall("[0-9]+",html)
+        nothing=patt[0]
+        print count,"\n",url
+        url=base_url+nothing
+        count+=1
+    except:
+        print "the last url is:\n"
+        print url,'\n',html
+        sys.exit(-1)
