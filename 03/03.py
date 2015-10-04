6d9453bb13625670dca1040aa0ad904b90a73841
zephyruslove <zyzengstorm@gmail.com>
2015-10-04 13:33:23 +0800

    python challenges

---
+++ 03/03.py
@@ -0,0 +1,15 @@
+#!/bin/bash
+#coding=utf-8
+
+import urllib,re
+url="http://www.pythonchallenge.com/pc/def/equality.html"
+html=urllib.urlopen(url).read()
+if not html:
+    print "url error"
+reg=re.compile("<!-(.*?)-->",re.S)
+answer=re.findall(reg,html)
+str=answer[0]
+patt=re.findall("[a-z]{1}[A-Z]{3}[a-z]{1}[A-Z]{3}[a-z]{1}",str)
+#print patt
+for i in patt:
+    print i[4]
