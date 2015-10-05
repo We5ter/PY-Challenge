952bdf966d0ff5753699d15c9caae2c804ca0e3b
zephyruslove <zyzengstorm@gmail.com>
2015-10-05 21:07:26 +0800

    python challenges

---
+++ 08/08.py
@@ -0,0 +1,8 @@
+#bin/bash
+#coding:utf-8
+#next:good.html,其实在源码里就能看到下一关的链接
+import bz2
+un='BZh91AY&SYA\xaf\x82\r\x00\x00\x01\x01\x80\x02\xc0\x02\x00 \x00!\x9ah3M\x07<]\xc9\x14\xe1BA\x06\xbe\x084'
+pw='BZh91AY&SY\x94$|\x0e\x00\x00\x00\x81\x00\x03$ \x00!\x9ah3M\x13<]\xc9\x14\xe1BBP\x91\xf08'
+print 'username:'+bz2.decompress(un)
+print 'password:'+bz2.decompress(pw)
