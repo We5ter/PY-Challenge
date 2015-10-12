aa5f142a74443da2b49ee3245212c3ee2fbce572
zephyruslove <zyzengstorm@gmail.com>
2015-10-12 22:08:41 +0800

    python challeng

---
+++ 16/16.py
@@ -0,0 +1,26 @@
+#!/bin/bash
+#coding:utf-8
+from PIL import Image
+
+im=Image.open("mozart.gif")
+
+#print im.size
+
+#print im.mode
+
+print im.getpixel((630,2))
+
+def straighten(line): 
+    i=0
+    while line[i]!=195:
+        i+=1
+    return line[i:]+line[:i]
+
+for h in range(im.size[1]):
+    line=[im.getpixel((w,h)) for w in range(im.size[0])]
+    line=straighten(line)
+    [im.putpixel((w,h),line[w]) for w in range(im.size[0])]
+
+im.save('answer.gif')
+
+print "done"
